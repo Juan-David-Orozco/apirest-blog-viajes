793b27bd51444c9c77ed6f09d700bb34299a28e0
Juan-David-Orozco <ing.ele2018@gmail.com>
2021-12-11 18:35:54 -0500

    endpoints publicaciones

--- apiapp.js
+++ apiapp.js
@@ -14,8 +14,58 @@ var pool = mysql.createPool({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (peticion, respuesta) => {
-  respuesta.send("Bienvenido")
+app.get('/api/v1/publicaciones', function(peticion, respuesta) {
+  
+  pool.getConnection(function(err, connection) {
+    let consulta
+    let modificadorConsulta = ""
+    const busqueda = ( peticion.query.busqueda ) ? peticion.query.busqueda : ""
+    if (busqueda != ""){
+      modificadorConsulta = `
+        WHERE
+        titulo LIKE '%${busqueda}%' OR
+        resumen LIKE '%${busqueda}%' OR
+        contenido LIKE '%${busqueda}%'
+      `
+    }
+    consulta = `
+      SELECT *
+      FROM publicaciones
+      ${modificadorConsulta}
+      ORDER BY id
+    `
+    connection.query(consulta, function(error, filas, campos) {
+      if(filas.length > 0){
+        respuesta.status(200)
+        respuesta.json({data: filas})
+      }
+      else{
+        respuesta.status(404)
+        respuesta.send({errors: ["No se encuentran publicaciones con ese criterio de busqueda"]})
+      }
+    })
+    connection.release()
+  })
+  
+})
+
+app.get('/api/v1/publicaciones/:id', function(peticion, respuesta) {
+  
+  pool.getConnection(function(err, connection) {
+    const query = `SELECT * FROM publicaciones WHERE id = ${connection.escape(peticion.params.id)}`
+    connection.query(query, function(error, filas, campos) {
+      if(filas.length > 0){
+        respuesta.status(200)
+        respuesta.json({data: filas[0]})
+      }
+      else{
+        respuesta.status(404)
+        respuesta.send({errors: ["No se encuentra esa publicación"]})
+      }
+    })
+    connection.release()
+  })
+  
 })
 
 app.listen(8000, function(){
