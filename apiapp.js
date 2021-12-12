b77f0ac9342b9899a8ede7d01dd7d6b27d7d8f12
Juan-David-Orozco <ing.ele2018@gmail.com>
2021-12-11 19:46:04 -0500

    endpoints autores

--- apiapp.js
+++ apiapp.js
@@ -68,6 +68,71 @@ app.get('/api/v1/publicaciones/:id', function(peticion, respuesta) {
   
 })
 
+app.get('/api/v1/autores', function(peticion, respuesta) {
+  
+  pool.getConnection(function(err, connection) {
+    const query = `SELECT email, pseudonimo FROM autores`
+    connection.query(query, function(error, filas, campos) {
+      if(filas.length > 0){
+        respuesta.status(200)
+        respuesta.json({data: filas})
+      }
+      else{
+        respuesta.status(404)
+        respuesta.send({errors: ["No se obtuvo ningun elemento"]})
+      }
+    })
+    connection.release()
+  })
+
+})
+
+app.get('/api/v1/autores/:id', function(peticion, respuesta) {
+  
+  pool.getConnection(function(err, connection) {
+    const query = `
+      SELECT autores.id id, email, pseudonimo, publicaciones.id publicacion_id, titulo, resumen, votos
+      FROM autores
+      LEFT JOIN
+      publicaciones ON
+      autores.id = publicaciones.autor_id
+      WHERE autores.id = ${connection.escape(peticion.params.id)}
+      ORDER BY autores.id DESC, publicaciones.fecha_hora DESC
+    `
+    connection.query(query, function(error, filas, campos) {
+      autor = []
+      AutorId = undefined
+      filas.forEach(registro => {
+        if (registro.id != AutorId){
+          AutorId = registro.id
+          autor.push({
+            id: registro.id,
+            pseudonimo: registro.pseudonimo,
+            email: registro.email,
+            publicaciones: []
+          })
+        }
+        autor[0].publicaciones.push({
+          id: registro.publicacion_id,
+          titulo: registro.titulo,
+          resumen: registro.resumen,
+          votos: registro.votos
+        })
+      });
+      if(autor.length > 0){
+        respuesta.status(200)
+        respuesta.json({data: autor})
+      }
+      else{
+        respuesta.status(404)
+        respuesta.send({errors: ["No se encuentra ese autor"]})
+      }
+    })
+    connection.release()
+  })
+  
+})
+
 app.listen(8000, function(){
   console.log("Servidor iniciado");
 })
